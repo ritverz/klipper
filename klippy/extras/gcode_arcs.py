6c7c26f561a7425453592916b5c7adc36c5a4f9f
Anton Negoda <anegoda@ritverc.com>
2023-08-04 15:21:41 +0300

    Cartesian_abc kinematics

--- klippy/extras/gcode_arcs.py
+++ klippy/extras/gcode_arcs.py
@@ -12,54 +12,87 @@ import math
 #
 # supports XY, XZ & YZ planes with remaining axis as helical
 
-# Enum
-ARC_PLANE_X_Y = 0
-ARC_PLANE_X_Z = 1
-ARC_PLANE_Y_Z = 2
-
-# Enum
-X_AXIS = 0
-Y_AXIS = 1
-Z_AXIS = 2
-E_AXIS = 3
-
-
 class ArcSupport:
 
     def __init__(self, config):
+        """Support for gcode arc (G2/G3) commands.
+        [gcode_arcs]
+        #resolution: 1.0
+        #   An arc will be split into segments. Each segment's length will
+        #   equal the resolution in mm set above. Lower values will produce a
+        #   finer arc, but also more work for your machine. Arcs smaller than
+        #   the configured value will become straight lines. The default is
+        #   1 mm.
+
+        To start adding support for multi-axis, this reads the 'axis' parameter
+        of the '[printer]' section in the config file:
+        [printer]
+        # ...
+        axis: XYZ  # Optional: XYZ or XYZABC
+        # ...
+        """
         self.printer = config.get_printer()
         self.mm_per_arc_segment = config.getfloat('resolution', 1., above=0.0)
 
         self.gcode_move = self.printer.load_object(config, 'gcode_move')
         self.gcode = self.printer.lookup_object('gcode')
-        self.gcode.register_command("G2", self.cmd_G2)
-        self.gcode.register_command("G3", self.cmd_G3)
+        
+        # Get amount of axes
+        # NOTE: Amount of non-extruder axes: XYZ=3, XYZABC=6.
+        self.axis_names = config.getsection("printer").get('axis', 'XYZ')  # "XYZ" / "XYZABC"
+        self.axis_count = len(self.axis_names)
 
+        # Enum
+        self.ARC_PLANE_X_Y = 0
+        self.ARC_PLANE_X_Z = 1
+        self.ARC_PLANE_Y_Z = 2
+
+        # Enum
+        self.X_AXIS = 0
+        self.Y_AXIS = 1
+        self.Z_AXIS = 2
+        self.E_AXIS = self.axis_count  # NOTE: Not used below.
+        
+        # Arc Move Clockwise.
+        self.gcode.register_command("G2", self.cmd_G2)
+        
+        # Arc Move Counter-clockwise.
+        self.gcode.register_command("G3", self.cmd_G3)
+        
+        # Arc Plane Select: G17 (XY plane), G18 (XZ plane), G19 (YZ plane).
         self.gcode.register_command("G17", self.cmd_G17)
         self.gcode.register_command("G18", self.cmd_G18)
         self.gcode.register_command("G19", self.cmd_G19)
 
+        # This is a named tuple with elements: ('x', 'y', 'z', 'e', 'a', 'b', 'c')
+        # Values default to None.
         self.Coord = self.gcode.Coord
 
         # backwards compatibility, prior implementation only supported XY
-        self.plane = ARC_PLANE_X_Y
+        self.plane = self.ARC_PLANE_X_Y
 
     def cmd_G2(self, gcmd):
+        """Arc Move Clockwise: G2 [X<pos>] [Y<pos>] [Z<pos>] [E<pos>] [F<speed>] I<value> J<value>|I<value> K<value>|J<value> K<value>"""
         self._cmd_inner(gcmd, True)
 
     def cmd_G3(self, gcmd):
+        """Arc Move Counter-clockwise: G3 [X<pos>] [Y<pos>] [Z<pos>] [E<pos>] [F<speed>] I<value> J<value>|I<value> K<value>|J<value> K<value>"""
         self._cmd_inner(gcmd, False)
 
     def cmd_G17(self, gcmd):
-        self.plane = ARC_PLANE_X_Y
+        """Arc Plane Select: G17 (XY plane)"""
+        self.plane = self.ARC_PLANE_X_Y
 
     def cmd_G18(self, gcmd):
-        self.plane = ARC_PLANE_X_Z
+        """Arc Plane Select: G18 (XZ plane)"""
+        self.plane = self.ARC_PLANE_X_Z
 
     def cmd_G19(self, gcmd):
-        self.plane = ARC_PLANE_Y_Z
+        """Arc Plane Select: G19 (YZ plane)"""
+        self.plane = self.ARC_PLANE_Y_Z
 
     def _cmd_inner(self, gcmd, clockwise):
+        # The arc's path is planned in absolute coordinates.
         gcodestatus = self.gcode_move.get_status()
         if not gcodestatus['absolute_coordinates']:
             raise gcmd.error("G2/G3 does not support relative move mode")
@@ -76,13 +109,13 @@ class ArcSupport:
 
         # determine the plane coordinates and the helical axis
         asPlanar = [ gcmd.get_float(a, 0.) for i,a in enumerate('IJ') ]
-        axes = (X_AXIS, Y_AXIS, Z_AXIS)
-        if self.plane == ARC_PLANE_X_Z:
+        axes = (self.X_AXIS, self.Y_AXIS, self.Z_AXIS)
+        if self.plane == self.ARC_PLANE_X_Z:
             asPlanar = [ gcmd.get_float(a, 0.) for i,a in enumerate('IK') ]
-            axes = (X_AXIS, Z_AXIS, Y_AXIS)
-        elif self.plane == ARC_PLANE_Y_Z:
+            axes = (self.X_AXIS, self.Z_AXIS, self.Y_AXIS)
+        elif self.plane == self.ARC_PLANE_Y_Z:
             asPlanar = [ gcmd.get_float(a, 0.) for i,a in enumerate('JK') ]
-            axes = (Y_AXIS, Z_AXIS, X_AXIS)
+            axes = (self.Y_AXIS, self.Z_AXIS, self.X_AXIS)
 
         if not (asPlanar[0] or asPlanar[1]):
             raise gcmd.error("G2/G3 requires IJ, IK or JK parameters")
@@ -91,8 +124,12 @@ class ArcSupport:
         asF = gcmd.get_float("F", None)
 
         # Build list of linear coordinates to move
-        coords = self.planArc(currentPos, asTarget, asPlanar,
-                              clockwise, *axes)
+        coords = self.planArc(currentPos=currentPos, 
+                              targetPos=asTarget, 
+                              offset=asPlanar,
+                              clockwise=clockwise,
+                              # Expand the axes list to pass its values to: "alpha_axis", "beta_axis", "helical_axis"
+                              *axes)
         e_per_move = e_base = 0.
         if asE is not None:
             if gcodestatus['absolute_extrude']:
@@ -167,8 +204,12 @@ class ArcSupport:
             r_P = -offset[0] * cos_Ti + offset[1] * sin_Ti
             r_Q = -offset[0] * sin_Ti - offset[1] * cos_Ti
 
-            # Coord doesn't support index assignment, create list
-            c = [None, None, None, None]
+            # Coord is a named tuple with elements: ('x', 'y', 'z', 'e', 'a', 'b', 'c')
+            # Its values default to None.
+            # Coord doesn't support index assignment, create list.
+            # NOTE: Using "axis_count" (e.g. can be "3" for an XYZ setup). Adding 1 to consider the Extruder axis. 
+            #       This achieves backwardcompatibility.
+            c = [None for i in range(self.axis_count + 1)]
             c[alpha_axis] = center_P + r_P
             c[beta_axis] = center_Q + r_Q
             c[helical_axis] = currentPos[helical_axis] + dist_Helical
